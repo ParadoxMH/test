2c87958befbb4f57c8bc27146d5787f68e87ada4
mhorditsa <mhorditsa@homeaway.com>
2019-10-17 14:15:37 +0300

    first commit1

---
+++ ConsoleAppForTests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace ConsoleAppForTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = "4/30/2020 4:29 PM";
+
+            var dt = DateTime.Parse(time);
+            var newDt = Round(dt);
+
+            Console.WriteLine(newDt);
+        }
+        public static int Round(DateTime dt)
+        {
+            return dt.Minute >= 30 ? dt.Hour + 1 : dt.Hour;
+        }
+    }
+}
+
